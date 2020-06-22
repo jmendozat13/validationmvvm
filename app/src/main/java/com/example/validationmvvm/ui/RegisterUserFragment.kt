34c8eb74e78dcdc9c8d31045a0ac27e7ed0aaff9
Jesus Valentin Mendoza Torres <jmendozat13@gmail.com>
2020-06-22 12:09:16 -0500

    implemented validation

--- app/src/main/java/com/example/validationmvvm/ui/RegisterUserFragment.kt
+++ app/src/main/java/com/example/validationmvvm/ui/RegisterUserFragment.kt
@@ -1,14 +1,17 @@
 package com.example.validationmvvm.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.validationmvvm.R
 import com.example.validationmvvm.databinding.RegisteruserFragmentBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class RegisterUserFragment : Fragment() {
@@ -22,13 +25,23 @@ class RegisterUserFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: RegisteruserFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.registeruser_fragment, container, false)
+        viewModel = ViewModelProvider(this).get(RegisterUserViewModel::class.java)
+        val binding: RegisteruserFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.registeruser_fragment, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(RegisterUserViewModel::class.java)
+        initObserver()
+    }
+
+    private fun initObserver() {
+        viewModel.errorMessage.observe(viewLifecycleOwner, Observer { message ->
+            Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG).show()
+        })
     }
 
 }
--- app/src/main/java/com/example/validationmvvm/ui/RegisterUserViewModel.kt
+++ app/src/main/java/com/example/validationmvvm/ui/RegisterUserViewModel.kt
@@ -1,14 +1,43 @@
 package com.example.validationmvvm.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RegisterUserViewModel : ViewModel() {
-    var firstName: String? = ""
-    var lastName: String? = ""
-    var email: String? = ""
-    var mobile: String? = ""
+    var firstName = MutableLiveData<String>()
+    var lastName = MutableLiveData<String>()
+    var email = MutableLiveData<String>()
+    var mobile = MutableLiveData<String>()
 
-    private fun validte() {
-        //TODO: place some logic here
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
+    fun acceptForm() {
+        if (validteForm()) {
+            Log.v("Fragment","acceptForm")
+        }
+    }
+
+    private fun validteForm(): Boolean {
+        if (firstName.value.isNullOrEmpty()) {
+            _errorMessage.value = "First name is required"
+            return false
+        }
+        if (lastName.value.isNullOrEmpty()) {
+            _errorMessage.value = "Last name is required"
+            return false
+        }
+        if (email.value.isNullOrEmpty()) {
+            _errorMessage.value = "Email is required"
+            return false
+        }
+        if (mobile.value.isNullOrEmpty()) {
+            _errorMessage.value = "Mobile is required"
+            return false
+        }
+        return true
     }
 }
